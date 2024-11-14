55aac673ad0c5af6fdbf677ca68ad6f5a11ab73e
raishivangi0922 <raishivangi943@gmail.com>
2024-11-14 16:22:44 +0530

    head

--- day2.py
+++ day2.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import streamlit as st
-df= pd.read_csv("Crime_Data_from_2020_to_Present.csv")
-st.dataframe(df.head())
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
+
+# Load the data
+df = pd.read_csv("Crime_Data_from_2020_to_Present.csv")
+
+st.dataframe(df.head(5))
---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import day2
+
+# Reading the data
+df = pd.read_csv("Crime_Data_from_2020_to_Present.csv")
